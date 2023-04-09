295bc0ed35bc2f7ace3208c33352f35d61441728
mdhar301 <127174149+mdhar301@users.noreply.github.com>
2023-04-09 13:55:35 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCMock.Models;
+using System.Diagnostics;
+using MVCMock.Repository;
+using MVCMock.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCMock.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStudentRepository repo;
+        private readonly DatabaseContext context;
+        public HomeController(IStudentRepository repo, DatabaseContext context)
+        {
+            this.repo = repo;
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var courses = context.Courses.ToList();
+            ViewBag.Courses = new SelectList(courses, "Id", "Name");
+            var students = context.Students.Include(s => s.StudentHobbies).ToList();
+            ViewBag.Hobbies = context.Hobbies.ToList();
+
+
+
+            return View(repo.GetStudents());
+        }
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            ViewBag.Courses = context.Courses.ToList();
+            ViewBag.Hobbies = context.Hobbies.ToList();
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStudent(Student student, int[] selectedHobbyIds) 
+        {
+            ViewBag.Courses = context.Courses.ToList();
+            ViewBag.Hobbies = context.Hobbies.ToList();
+
+            var validator = new StudentValidator();
+            var result = validator.Validate(student);
+            if (!result.IsValid)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                var hobbies = repo.GetAllHobbies();
+                return View(student);
+            }
+            var selectedHobbies = repo.GetHobbiesByIds(selectedHobbyIds);
+            student.StudentHobbies = selectedHobbies.Select(h => new StudentHobby { Student = student, Hobbies = h }).ToList();
+            var students = context.Students.Include(s => s.StudentHobbies).ToList();
+
+            repo.AddStudent(student);
+            return RedirectToAction("Index");
+            
+
+        }
+        [HttpGet]
+        public IActionResult EditStudent(int id)
+        {
+            var student = context.Students
+        .Include(x => x.StudentHobbies)
+        .FirstOrDefault(x => x.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Courses = context.Courses.ToList();
+            ViewBag.Hobbies = context.Hobbies.ToList();
+            var selectedHobbies = student.StudentHobbies.Select(x => x.Hobbies).ToArray();
+            ViewBag.SelectedHobbyIds = selectedHobbies;
+            return View(repo.GetStudentById(id));
+        }
+    [HttpPost]
+    public IActionResult EditStudent(int id, Student student, int[] selectedHobbyIds)
+    {
+        var validator = new StudentValidator();
+        var result = validator.Validate(student);
+        if (!result.IsValid)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+            ViewBag.Courses = context.Courses.ToList();
+            ViewBag.Hobbies = context.Hobbies.ToList();
+            return View(student);
+        }
+            repo.UpdateStudent(id, student, selectedHobbyIds);
+            return RedirectToAction("Index");     
+    
+    }
+
+
+        [HttpGet]
+
+        public IActionResult DeleteStudent(int id)
+        {
+            repo.DeleteStudent(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCMock.Models;
+using System.Collections.Generic;
+
+namespace MVCMock.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Hobbies> Hobbies { get; set; }
+        public DbSet<StudentHobby> StudentHobbies { get; set; }
+
+
+
+    }
+}
